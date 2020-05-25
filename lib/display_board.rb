817522f6da4661a7eb86963d5b08466f06ace078
Mario Terrazas <mtrrzs@hotmail.com>
2020-05-25 19:30:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,9 @@
+ def display_board
+ puts "Welcome to Tic Tac Toe"
+ puts "      |       |      "
+ puts "_ _ _ _ _ _ _ _ _ _ _"
+ puts "      |       |      "
+ puts "_ _ _ _ _ _ _ _ _ _ _"
+ puts "      |       |      "
+ end
  
- 
-
-
-
